9ac30634e2aece7196c7fce7eb4ef4ac4084bf27
steven1306 <stev.valderrama@udla.edu.co>
2023-12-07 23:37:45 -0500

    primera actualizacion

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:qr_reader/pages/home_page.dart';
+import 'package:qr_reader/pages/mapa_page.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'QR reader',
+      initialRoute: 'home',
+      routes: {'home': (_) => HomeScreen(), 'mapa': (_) => Mapapage()},
+      theme: ThemeData(
+          primaryColor: Colors.amber,
+          floatingActionButtonTheme:
+              FloatingActionButtonThemeData(backgroundColor: Colors.amber)),
+    );
+  }
+}
